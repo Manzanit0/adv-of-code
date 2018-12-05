4cac9fed2867b3a3b23658f98e6fb4c64f047d3c
Manzanit0 <unico.javier@gmail.com>
2018-12-05 00:31:31 +0000

    Day 3 - part 1.

--- aoc2018/app.py
+++ aoc2018/app.py
@@ -1,5 +1,6 @@
 from day01 import calibrator
 from day02 import checksum
+from day03 import solution
 
 # # Day 1
 # d1p1 = calibrator.solve_part1('day01/input')
@@ -9,8 +10,12 @@ from day02 import checksum
 # print(d1p2)
 
 # Day 2
-d2p1 = checksum.solve_part1('day02/input')
-print(d2p1)
+# d2p1 = checksum.solve_part1('day02/input')
+# print(d2p1)
 
-d2p2 = checksum.solve_part2('day02/input')
-print(d2p2)
+# d2p2 = checksum.solve_part2('day02/input')
+# print(d2p2)
+
+# Day 3
+d3p1 = solution.solve_part1('day03/input')
+print(d3p1)
